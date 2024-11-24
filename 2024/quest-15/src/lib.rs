6f816b61e1dae478a0b503861bc9d7e4da42f504
Vesco Claudio <claudio.vesco@gmail.com>
2024-11-24 23:02:50 +0100

    quest-15

--- 2024/quest-15/src/lib.rs
+++ 2024/quest-15/src/lib.rs
@@ -1,17 +1,73 @@
+use std::collections::VecDeque;
+
+type Point = (usize, usize);
+
+/// # Panics
 #[must_use]
-pub fn part_1(_data: &[u8]) -> u64 {
-    todo!()
+pub fn solve(data: &[u8]) -> usize {
+    let map = data.split(|&c| c == b'\n').collect::<Vec<_>>();
+    let width = map[0].len();
+    let height = map.len();
+
+    let start: Point = (0, map[0].iter().position(|&c| c == b'.').unwrap());
+
+    let target = data.iter().fold(0_u32, |acc, &c| {
+        if c.is_ascii_uppercase() {
+            acc | 1 << (c - b'A')
+        } else {
+            acc
+        }
+    });
+
+    let key = |(r, c), target| target as usize * width * height + r * width + c;
+
+    let mut visited = vec![false; height * width * (target as usize + 1)];
+    visited[key(start, target)] = true;
+
+    let mut queue = VecDeque::new();
+    queue.push_back((start, 0, target));
+
+    while let Some(((r, c), distance, target)) = queue.pop_front() {
+        if target == 0 && start.0 == r && start.1 == c {
+            return distance;
+        }
+
+        [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            .iter()
+            .filter_map(
+                |(dr, dc)| match (r.checked_add_signed(*dr), c.checked_add_signed(*dc)) {
+                    (Some(r), Some(c)) => match map.get(r).and_then(|row| row.get(c)) {
+                        _ if visited[key((r, c), target)] => None,
+                        Some(tile) if tile.is_ascii_uppercase() => {
+                            visited[key((r, c), target)] = true;
+
+                            if target & (1 << (tile - b'A')) != 0 {
+                                let target = target ^ (1 << (tile - b'A'));
+                                Some(((r, c), target))
+                            } else {
+                                Some(((r, c), target))
+                            }
+                        }
+                        Some(b'.') => {
+                            visited[key((r, c), target)] = true;
+                            Some(((r, c), target))
+                        }
+                        _ => None,
+                    },
+                    _ => None,
+                },
+            )
+            .for_each(|(next, target)| {
+                queue.push_back((next, distance + 1, target));
+            });
+    }
+
+    unreachable!()
 }
 
-#[must_use]
-pub fn part_2(_data: &[u8]) -> u64 {
-    todo!()
-}
-
-#[must_use]
-pub fn part_3(_data: &[u8]) -> u64 {
-    todo!()
-}
+pub use solve as part_1;
+pub use solve as part_2;
+pub use solve as part_3;
 
 #[cfg(test)]
 mod tests {
@@ -19,16 +75,36 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(5, part_1(br""));
+        assert_eq!(
+            26,
+            solve(
+                br"#####.#####
+#.........#
+#.######.##
+#.........#
+###.#.#####
+#H.......H#
+###########"
+            )
+        );
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(28, part_2(br""));
-    }
-
-    #[test]
-    fn test_part_3() {
-        assert_eq!(30, part_3(br""));
+        assert_eq!(
+            38,
+            solve(
+                br"##########.##########
+#...................#
+#.###.##.###.##.#.#.#
+#..A#.#..~~~....#A#.#
+#.#...#.~~~~~...#.#.#
+#.#.#.#.~~~~~.#.#.#.#
+#...#.#.B~~~B.#.#...#
+#...#....BBB..#....##
+#C............#....C#
+#####################"
+            )
+        );
     }
 }
