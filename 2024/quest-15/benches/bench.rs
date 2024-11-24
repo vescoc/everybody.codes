7aa3342c3eae0374a19c12aacfdfc8d76cdeba87
Vesco Claudio <claudio.vesco@gmail.com>
2024-11-24 23:08:36 +0100

    bench: reduced samples to 10 for part 3

--- 2024/quest-15/benches/bench.rs
+++ 2024/quest-15/benches/bench.rs
@@ -3,15 +3,18 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use quest_15 as quest;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("simple", |b| {
+    c.bench_function("part 1", |b| {
         b.iter(|| quest::part_1(include_bytes!("../data/part_1")))
     });
-    c.bench_function("complex", |b| {
+    c.bench_function("part 2", |b| {
         b.iter(|| quest::part_2(include_bytes!("../data/part_2")))
     });
-    c.bench_function("target", |b| {
+
+    let mut group = c.benchmark_group("part_3");
+    group.sample_size(10).bench_function("part 3", |b| {
         b.iter(|| quest::part_3(include_bytes!("../data/part_3")))
     });
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
