fa281ad8d58c0fdab1705f61be56301958315c92
Vesco Claudio <claudio.vesco@gmail.com>
2025-08-27 18:29:04 +0200

    story 2/01/3: optimizations

--- stories/2/quest-01/benches/bench.rs
+++ stories/2/quest-01/benches/bench.rs
@@ -10,11 +10,20 @@ fn criterion_benchmark(c: &mut Criterion) {
     group.bench_function("part 2", |b| {
         b.iter(|| story::part_2(include_str!("../data/part_2")));
     });
-    group.bench_function("part 3", |b| {
-        b.iter(|| story::part_3(include_str!("../data/part_3")));
+    group.bench_function("part 3 fast", |b| {
+        b.iter(|| story::part_3_fast(include_str!("../data/part_3")));
     });
     group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+fn criterion_benchmark_slow(c: &mut Criterion) {
+    let mut group = c.benchmark_group("story-2-01");
+    group.sample_size(10);
+    group.bench_function("part 3 bf", |b| {
+        b.iter(|| story::part_3_bf(include_str!("../data/part_3")));
+    });
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark, criterion_benchmark_slow);
 criterion_main!(benches);
--- stories/2/quest-01/src/lib.rs
+++ stories/2/quest-01/src/lib.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::iter::Peekable;
+
 use rayon::prelude::*;
 
 use itertools::Itertools;
@@ -71,9 +74,11 @@ pub fn part_2(data: &str) -> u64 {
         .sum()
 }
 
+pub use part_3_fast as part_3;
+
 /// # Panics
 #[must_use]
-pub fn part_3(data: &str) -> String {
+pub fn part_3_bf(data: &str) -> String {
     let (pattern, tokens) = data.split_once("\n\n").unwrap();
     let pattern = pattern.lines().collect::<Vec<_>>();
 
@@ -102,6 +107,168 @@ pub fn part_3(data: &str) -> String {
         .unwrap();
 
     format!("{min} {max}")
+}
+
+/// # Panics
+#[must_use]
+pub fn part_3_fast(data: &str) -> String {
+    let (pattern, tokens) = data.split_once("\n\n").unwrap();
+    let pattern = pattern.lines().collect::<Vec<_>>();
+
+    let slots = pattern[0].len() / 2;
+
+    let games = tokens
+        .par_lines()
+        .map(|sequence| {
+            let mut throws = (0..=slots)
+                .map(|toss_slot| token_fall(&pattern, sequence, toss_slot))
+                .enumerate()
+                .collect::<Vec<_>>();
+            throws.sort_unstable_by_key(|(_, v)| *v);
+            throws
+        })
+        .collect::<Vec<_>>();
+
+    let min = min_coins(&games);
+    let max = max_coins(&games);
+
+    format!("{min} {max}")
+}
+
+fn min_coins(games: &[Vec<(usize, u64)>]) -> u64 {
+    calc_coins::<{ u64::MAX }, _, _, _, _>(|a, b| a > b, std::cmp::Ord::min, |ii| ii, games)
+}
+
+fn max_coins(games: &[Vec<(usize, u64)>]) -> u64 {
+    calc_coins::<{ u64::MIN }, _, _, _, _>(
+        |a, b| a < b,
+        std::cmp::Ord::max,
+        std::iter::Iterator::rev,
+        games,
+    )
+}
+
+fn calc_coins<const INIT: u64, IOUT, CUT, MINMAX, REV>(
+    cut: CUT,
+    minmax: MINMAX,
+    rev: REV,
+    games: &[Vec<(usize, u64)>],
+) -> u64
+where
+    IOUT: Iterator<Item = (usize, u64)> + Clone,
+    CUT: Fn(u64, u64) -> bool + Copy,
+    MINMAX: Fn(u64, u64) -> u64 + Copy,
+    REV: Fn(std::vec::IntoIter<(usize, u64)>) -> IOUT,
+{
+    let mut current = INIT;
+
+    let mut current_coins = 0;
+    let mut set = Box::new([0u8; 20]);
+    let mut candidate = HashMap::<usize, Vec<_>>::with_capacity(6);
+    for (token_id, token_throws) in games.iter().enumerate() {
+        let mut token_throws = rev(token_throws.clone().into_iter()).peekable();
+        let (slot, coins) = *token_throws.peek().unwrap();
+        current_coins += coins;
+        candidate
+            .entry(slot)
+            .or_default()
+            .push((token_id, token_throws));
+        set[slot] |= 1 << token_id;
+    }
+
+    let mut seen = HashMap::new();
+
+    calc_coins_r::<INIT, _, _, _>(
+        cut,
+        minmax,
+        &mut current,
+        &mut seen,
+        current_coins,
+        set,
+        &candidate,
+    )
+}
+
+fn calc_coins_r<const INIT: u64, I, CUT, MINMAX>(
+    cut: CUT,
+    minmax: MINMAX,
+    current: &mut u64,
+    seen: &mut HashMap<Box<[u8; 20]>, u64>,
+    coins: u64,
+    set: Box<[u8; 20]>,
+    candidate: &HashMap<usize, Vec<(usize, Peekable<I>)>>,
+) -> u64
+where
+    I: Iterator<Item = (usize, u64)> + Clone,
+    CUT: Fn(u64, u64) -> bool + Copy,
+    MINMAX: Fn(u64, u64) -> u64 + Copy,
+{
+    if cut(coins, *current) {
+        return coins;
+    }
+
+    if seen.contains_key(&set) {
+        return seen[&set];
+    }
+
+    if candidate.len() == 6 {
+        seen.insert(set, coins);
+        *current = minmax(*current, coins);
+        return coins;
+    }
+
+    let mut new_current = INIT;
+    for (slot, tokens) in candidate {
+        if tokens.len() > 1 {
+            let old_slot_coins = tokens
+                .iter()
+                .filter_map(|(_, token_throws)| {
+                    token_throws.clone().peek().map(|(_, coins)| *coins)
+                })
+                .sum::<u64>();
+            for (token_id, token_throws) in tokens {
+                let mut token_throws = token_throws.clone();
+                let mut new_coins = token_throws.peek().unwrap().1;
+
+                let mut new_candidate = candidate.clone();
+                new_candidate.insert(*slot, vec![(*token_id, token_throws)]);
+
+                let mut new_set = set.clone();
+                new_set[*slot] = 1 << token_id;
+
+                for (other_token_id, other_token_throws) in tokens {
+                    if other_token_id != token_id {
+                        let mut other_token_throws = other_token_throws.clone();
+                        let _ = other_token_throws.next();
+                        let (new_token_slot, new_token_coins) = *other_token_throws.peek().unwrap();
+                        new_candidate
+                            .entry(new_token_slot)
+                            .or_default()
+                            .push((*other_token_id, other_token_throws));
+                        new_coins += new_token_coins;
+                        new_set[new_token_slot] |= 1 << other_token_id;
+                    }
+                }
+
+                new_current = minmax(
+                    new_current,
+                    calc_coins_r::<INIT, I, _, _>(
+                        cut,
+                        minmax,
+                        current,
+                        seen,
+                        coins + new_coins - old_slot_coins,
+                        new_set,
+                        &new_candidate,
+                    ),
+                );
+            }
+        }
+    }
+
+    seen.insert(set, new_current);
+
+    new_current
 }
 
 #[cfg(test)]
@@ -174,7 +341,7 @@ RLLRRRRLRLRLRLRLLRRL";
     }
 
     #[test]
-    fn test_part_3_1() {
+    fn test_part_3_1_bf() {
         let data = r"*.*.*.*.*.*.*.*.*
 .*.*.*.*.*.*.*.*.
 *.*.*...*.*...*..
@@ -195,11 +362,11 @@ LRLLLRRRLRLR
 LLRLLRLLLRRL
 LRLRLLLRRRRL";
 
-        assert_eq!(&part_3(data), "13 43");
+        assert_eq!(&part_3_bf(data), "13 43");
     }
 
     #[test]
-    fn test_part_3_2() {
+    fn test_part_3_2_bf() {
         let data = r"*.*.*.*.*.*.*.*.*.*.*.*.*
 .*.*.*.*.*.*.*.*.*.*.*.*.
 ..*.*.*.*...*.*...*.*.*..
@@ -232,7 +399,7 @@ LLLLLLLLRLLRRLLRLLLL";
     }
 
     #[test]
-    fn test_part_3_3() {
+    fn test_part_3_3_bf() {
         let data = r"*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*
 .*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.
 ..*.*.*.*.*.*.........*.*.*.*.....*.*.*
@@ -262,5 +429,96 @@ LRRRRLRRLRLLRLLRRLRR
 RRRRRRRRLRRRRLLRRRLR";
 
         assert_eq!(&part_3(data), "39 122");
+    }
+
+    #[test]
+    fn test_part_3_1_fast() {
+        let data = r"*.*.*.*.*.*.*.*.*
+.*.*.*.*.*.*.*.*.
+*.*.*...*.*...*..
+.*.*.*.*.*...*.*.
+*.*.....*...*.*.*
+.*.*.*.*.*.*.*.*.
+*...*...*.*.*.*.*
+.*.*.*.*.*.*.*.*.
+*.*.*...*.*.*.*.*
+.*...*...*.*.*.*.
+*.*.*.*.*.*.*.*.*
+.*.*.*.*.*.*.*.*.
+
+RRRLRLRRRRRL
+LLLLRLRRRRRR
+RLLLLLRLRLRL
+LRLLLRRRLRLR
+LLRLLRLLLRRL
+LRLRLLLRRRRL";
+
+        assert_eq!(&part_3_fast(data), "13 43");
+    }
+
+    #[test]
+    fn test_part_3_2_fast() {
+        let data = r"*.*.*.*.*.*.*.*.*.*.*.*.*
+.*.*.*.*.*.*.*.*.*.*.*.*.
+..*.*.*.*...*.*...*.*.*..
+.*...*.*.*.*.*.*.....*.*.
+*.*...*.*.*.*.*.*...*.*.*
+.*.*.*.*.*.*.*.*.......*.
+*.*.*.*.*.*.*.*.*.*...*..
+.*.*.*.*.*.*.*.*.....*.*.
+*.*...*.*.*.*.*.*.*.*....
+.*.*.*.*.*.*.*.*.*.*.*.*.
+*.*.*.*.*.*.*.*.*.*.*.*.*
+.*.*.*.*.*.*.*.*.*...*.*.
+*.*.*.*.*.*.*.*.*...*.*.*
+.*.*.*.*.*.*.*.*.....*.*.
+*.*.*.*.*.*.*.*...*...*.*
+.*.*.*.*.*.*.*.*.*.*.*.*.
+*.*.*...*.*.*.*.*.*.*.*.*
+.*...*.*.*.*...*.*.*...*.
+*.*.*.*.*.*.*.*.*.*.*.*.*
+.*.*.*.*.*.*.*.*.*.*.*.*.
+
+RRRLLRRRLLRLRRLLLRLR
+RRRRRRRRRRLRRRRRLLRR
+LLLLLLLLRLRRLLRRLRLL
+RRRLLRRRLLRLLRLLLRRL
+RLRLLLRRLRRRLRRLRRRL
+LLLLLLLLRLLRRLLRLLLL";
+
+        assert_eq!(&part_3_fast(data), "25 66");
+    }
+
+    #[test]
+    fn test_part_3_3_fast() {
+        let data = r"*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*
+.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.
+..*.*.*.*.*.*.........*.*.*.*.....*.*.*
+.*.*...*.*.*.*.*.*.*.*.*.*.*...*.*.*.*.
+*.*.*.*...*.*.*.*.*.....*.*.*.*...*.*..
+.*...*.*...*.*.*.*.*.*.*.....*.*.*.*.*.
+*.*.*.*.*.....*.*.*.*.*.*.*.*.*.*.*.*.*
+.*.*.*.*.*.*...*.*.*.*.....*.*.*.*...*.
+*.*...*.*.*.*.*.*.*.*...*.*.*...*.*.*.*
+.*...*.*.*.*.*.*.*.*...*.*.*.*.*.*.*.*.
+*.*.*.*.*.*...*.....*.*...*...*.*.*.*.*
+.*...*.*.*.*.*...*.*.*.*.*...*.*...*.*.
+*.*.*.*.*...*.*.*.*.*.*.*.*...*.*.*.*.*
+.*.*.*.*.*.*.*.*...*.*.*.*.*.*.*.*.*.*.
+....*.*.*.*...*.*.*.*.*.*.*...*.*.*...*
+.*.*.*...*.*.*.*.*...*.*.*.*.*.*.*.*...
+*.*.*.*.*.*.*.....*...*...*.*.*.*.*.*.*
+.*.*...*.....*.*.*.*.*.*.*...*.*.*.*.*.
+*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*
+.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.
+
+RRRRLLRRLLLLLLLRLLRL
+RRRRRRRLRRLRRLRRRLRR
+RRRLLRRRRRLRRRRRLRRR
+LLLLRRLLRRLLLLLRRLLL
+LRRRRLRRLRLLRLLRRLRR
+RRRRRRRRLRRRRLLRRRLR";
+
+        assert_eq!(&part_3_fast(data), "39 122");
     }
 }
