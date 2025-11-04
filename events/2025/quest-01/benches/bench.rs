a2366472bccbdb2f366600d3dbed66467cd417cc
Claudio Vesco <claudio@gaia.intranet.vesco.org>
2025-11-04 05:17:26 +0100

    2025/quest-01

---
+++ events/2025/quest-01/benches/bench.rs
@@ -0,0 +1,20 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+
+use event_2025_01 as event;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("event-2025-01");
+    group.bench_function("part 1", |b| {
+        b.iter(|| event::part_1(include_str!("../data/part_1")))
+    });
+    group.bench_function("part 2", |b| {
+        b.iter(|| event::part_2(include_str!("../data/part_2")))
+    });
+    group.bench_function("part 3", |b| {
+        b.iter(|| event::part_3(include_str!("../data/part_3")))
+    });
+    group.finish();
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
---
+++ events/2025/quest-01/src/lib.rs
@@ -0,0 +1,112 @@
+/// # Panics
+#[must_use]
+pub fn part_1(data: &str) -> &str {
+    let (list, moves) = data.split_once("\n\n").expect("invalid data");
+
+    let list = list.split(',').collect::<Vec<_>>();
+
+    let index = moves.split(',').fold(0usize, |current, current_move| {
+        let mut mv = current_move.chars();
+        let dir = mv.next();
+        let steps = mv.as_str().parse::<usize>().expect("invalid move steps");
+        match dir {
+            Some('R') => current.saturating_add(steps).min(list.len() - 1),
+            Some('L') => current.saturating_sub(steps),
+            _ => panic!("invalid move"),
+        }
+    });
+
+    list[index]
+}
+
+/// # Panics
+#[must_use]
+pub fn part_2(data: &str) -> &str {
+    let (list, moves) = data.split_once("\n\n").expect("invalid data");
+
+    let list = list.split(',').collect::<Vec<_>>();
+
+    let index = moves.split(',').fold(0usize, |current, current_move| {
+        let mut mv = current_move.chars();
+        let dir = mv.next();
+        let steps = mv.as_str().parse::<usize>().expect("invalid move steps");
+        match dir {
+            Some('R') => current.saturating_add(steps),
+            Some('L') => {
+                if steps >= current {
+                    list.len() - (steps - current).rem_euclid(list.len())
+                } else {
+                    current - steps
+                }
+            }
+            _ => panic!("invalid move"),
+        }
+        .rem_euclid(list.len())
+    });
+
+    list[index]
+}
+
+/// # Panics
+#[must_use]
+pub fn part_3(data: &str) -> &str {
+    let (list, moves) = data.split_once("\n\n").expect("invalid data");
+
+    let mut list = list.split(',').collect::<Vec<_>>();
+    for current_move in moves.split(',') {
+        let mut mv = current_move.chars();
+        let dir = mv.next();
+        let steps = mv.as_str().parse::<usize>().expect("invalid move steps");
+        let target = match dir {
+            Some('R') => steps,
+            Some('L') => list.len() - steps.rem_euclid(list.len()),
+            _ => panic!("invalid move"),
+        }
+        .rem_euclid(list.len());
+
+        list.swap(0, target);
+    }
+
+    list[0]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            part_1(
+                r"Vyrdax,Drakzyph,Fyrryn,Elarzris
+
+R3,L2,R3,L1"
+            ),
+            "Fyrryn",
+        )
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            part_2(
+                r"Vyrdax,Drakzyph,Fyrryn,Elarzris
+
+R3,L2,R3,L1"
+            ),
+            "Elarzris",
+        )
+    }
+
+    #[test]
+    fn test_part_3() {
+        assert_eq!(
+            part_3(
+                r"Vyrdax,Drakzyph,Fyrryn,Elarzris
+
+R3,L2,R3,L3"
+            ),
+            "Drakzyph",
+        )
+    }
+}
