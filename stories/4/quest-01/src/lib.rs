9cc8519a8c72884c0b00e06fbb84e050b50b42c8
Vesco Claudio <claudio.vesco@gmail.com>
2026-08-26 02:31:04 +0200

    story 4 / quest 1

--- stories/4/quest-01/src/lib.rs
+++ stories/4/quest-01/src/lib.rs
@@ -1,19 +1,149 @@
-/// # Panics
-#[must_use]
-pub fn part_1(_data: &str) -> u64 {
-    todo!()
+//! [Story 4 Quest 1](https://everybody.codes/story/4/quests/1)
+
+use std::collections::HashSet;
+
+use rayon::prelude::*;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("invalid number")]
+    InvalidNumber,
 }
 
-/// # Panics
-#[must_use]
-pub fn part_2(_data: &str) -> u64 {
-    todo!()
+/// Resolve part 1
+///
+/// # Errors
+///
+/// * [`Error::InvalidNumber`] - if a jump is invalid.
+pub fn part_1(data: &str) -> Result<i32, Error> {
+    data.par_lines()
+        .map(|line| {
+            let mut visited = HashSet::from([0]);
+            line.split(',').try_fold(0, move |current, jump| {
+                let jump = jump.parse::<i32>().map_err(|_| Error::InvalidNumber)?;
+
+                let candidate = current - jump;
+                if candidate >= 0 && visited.insert(candidate) {
+                    return Ok(candidate);
+                }
+
+                let candidate = current + jump;
+                visited.insert(candidate);
+
+                Ok(candidate)
+            })
+        })
+        .sum()
 }
 
-/// # Panics
-#[must_use]
-pub fn part_3(_data: &str) -> u64 {
-    todo!()
+/// Resolve part 2
+///
+/// # Errors
+///
+/// * [`Error::InvalidNumber`] - if a jump is invalid.
+pub fn part_2(data: &str) -> Result<i32, Error> {
+    data.par_lines()
+        .map(|line| {
+            let mut visited = HashSet::from([0]);
+            line.split(',').try_fold(0, move |current, jump| {
+                let jump = jump.parse::<i32>().map_err(|_| Error::InvalidNumber)?;
+
+                let candidate = current - jump;
+                if candidate >= 0 && visited.insert(candidate) {
+                    return Ok(candidate);
+                }
+
+                let mut candidate = current + jump;
+                while !visited.insert(candidate) {
+                    candidate += 1;
+                }
+
+                Ok(candidate)
+            })
+        })
+        .sum()
+}
+
+/// Check if the candidate arc cross the existing arcs.
+///
+/// # Arguments
+///
+/// * `arcs` - list of exists arcs
+/// * `(candidate_start, candidate_end)` - the arc to check.
+///
+/// # Returns
+///
+/// `true` if the candidate arc cross any of existing arcs
+fn arc_cross(arcs: &[(i32, i32)], (candidate_start, candidate_end): (i32, i32)) -> bool {
+    arcs.iter().any(|&(arc_start, arc_end)| {
+        ((arc_start..=arc_end).contains(&candidate_start)
+            && !(arc_start..=arc_end).contains(&candidate_end))
+            || ((arc_start..=arc_end).contains(&candidate_end)
+                && !(arc_start..=arc_end).contains(&candidate_start))
+    })
+}
+
+/// Resolve part 3
+///
+/// # Errors
+///
+/// * [`Error::InvalidNumber`] - if a jump is invalid.
+pub fn part_3(data: &str) -> Result<i32, Error> {
+    data.par_lines()
+        .map(|line| {
+            // Set of visited edges
+            let mut visited = HashSet::from([0]);
+
+            // List of existing arcs. In index 0 the *lower* arcs. In
+            // index 1 the *upper* arcs
+            let mut arcs = [vec![], vec![]];
+
+            // Current arc type:
+            // 0 - *lower*/*down* arc
+            // 1 - *upper* arc
+            let mut arc_type = 0;
+
+            // Max edge index on the right
+            let mut max_edge = 0;
+            line.split(',').try_fold(0, move |current, jump| {
+                let jump = jump.parse::<i32>().map_err(|_| Error::InvalidNumber)?;
+
+                let candidate = current - jump;
+                if candidate >= 0
+                    && !visited.contains(&candidate)
+                    && !arc_cross(&arcs[arc_type], (candidate, current))
+                {
+                    visited.insert(candidate);
+                    arcs[arc_type].push((candidate, current));
+                    max_edge = max_edge.max(current);
+
+                    arc_type = (arc_type + 1) % 2;
+
+                    return Ok(candidate);
+                }
+
+                let mut candidate = current + jump;
+                loop {
+                    if !visited.contains(&candidate)
+                        && !arc_cross(&arcs[arc_type], (current, candidate))
+                    {
+                        visited.insert(candidate);
+                        arcs[arc_type].push((current, candidate));
+                        max_edge = max_edge.max(candidate);
+
+                        arc_type = (arc_type + 1) % 2;
+
+                        return Ok(candidate);
+                    }
+
+                    candidate += 1;
+                    if candidate > max_edge + 1 {
+                        return Ok(current);
+                    }
+                }
+            })
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -21,21 +151,57 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_part_1() {
-        let data = r"";
-        assert_eq!(part_1(data), 9166);
+    fn test_part_1_1() {
+        let data = r"1,2,3,4,5,6,7,8,9
+1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30";
+        assert_eq!(part_1(data).unwrap(), 66);
+    }
+
+    #[test]
+    fn test_part_1_2() {
+        let data = r"1,1,1,1,1
+5,1,2,3,4,5,1,2,3,4
+2,1,1,2,1,1,2,1,1,2,1,1
+5,1,2,1,2,7,1,2,1,2,7,1,2,1,2";
+        assert_eq!(part_1(data).unwrap(), 34);
     }
 
     #[test]
     fn test_part_2() {
-        let data = r"";
-        assert_eq!(part_2(data), 2456);
+        let data = r"1,1,1,1,1
+5,1,2,3,4,5,1,2,3,4
+2,1,1,2,1,1,2,1,1,2,1,1
+5,1,2,1,2,7,1,2,1,2,7,1,2,1,2";
+        assert_eq!(part_2(data).unwrap(), 43);
     }
 
-    #[allow(clippy::unreadable_literal)]
     #[test]
-    fn test_part_3() {
-        let data = r"";
-        assert_eq!(part_3(data), 292320);
+    fn test_part_3_1() {
+        let data = r"1,1,1,1,1
+5,1,2,3,4,5,1,2,3,4
+2,1,1,2,1,1,2,1,1,2,1,1
+5,1,2,1,2,7,1,2,1,2,7,1,2,1,2";
+        assert_eq!(part_3(data).unwrap(), 27);
+    }
+
+    #[test]
+    fn test_part_3_2() {
+        let data = r"5,3,1,1
+5,3,1,1,5,1,1,3,4,8,1,1
+5,3,1,1,5,1,1,3,4,8,2,1
+10,9,9,8,8,7,7,6,6,5,5,4,4,3,3,2,2,1";
+        assert_eq!(part_3(data).unwrap(), 35);
+    }
+
+    #[test]
+    fn test_part_3_2_1() {
+        let data = r"5,3,1,1";
+        assert_eq!(part_3(data).unwrap(), 6);
+    }
+
+    #[test]
+    fn test_part_3_2_2() {
+        let data = r"5,3,1,1,5,1,1,3,4,8,1,1";
+        assert_eq!(part_3(data).unwrap(), 17);
     }
 }
